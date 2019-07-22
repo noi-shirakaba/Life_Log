1d87138f147b12590ff3b543ec1022b3a7ecd288
hogehoge <hogehoge@g>
2019-07-22 14:49:07 +0900

    first commit

---
+++ front/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // options...
+    devServer: {
+        watchOptions: {
+          ignored: /node_modules/,
+          aggregateTimeout: 300,
+          poll: 1000,
+        },
+    }
+}
