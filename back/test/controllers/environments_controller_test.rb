1a8e700347a2740e20b56e57b74759fc7d817835
HI <inoue15451116@gmail.com>
2019-08-15 14:11:13 +0900

    テストの削除

--- back/test/controllers/environments_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EnvironmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get environments_index_url
-    assert_response :success
-  end
-
-end
