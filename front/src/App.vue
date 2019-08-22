baa1162f26e8b6166d08ceb4801db7cfc60fabbb
HI <inoue15451116@gmail.com>
2019-08-22 16:02:43 +0900

    不要な部分を削除

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <Form></Form>
-    <Index></Index>
   </div>
 </template>
 
 <script>
 import Form from './components/form.vue'
-import Index from './components/index.vue'
 
 export default {
   name: 'App',
   components: {
-    Form,
-    Index
+    Form
   }
 }
 </script>
