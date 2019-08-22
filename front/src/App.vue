78269835388de040a9160748e8c0b22ba80d5ba5
HI <inoue15451116@gmail.com>
2019-08-22 15:26:01 +0900

    indexでdata取得、表示

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Form></Form>
+    <Index></Index>
   </div>
 </template>
 
 <script>
 import Form from './components/form.vue'
+import Index from './components/index.vue'
 
 export default {
   name: 'App',
   components: {
-    Form
+    Form,
+    Index
   }
 }
 </script>
---
+++ front/src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'From',
+  data(){
+    return {
+      situation: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
