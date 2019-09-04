5ae1c1ed9e0dbc7e5d13dfa7f84d678af111cdf4
H I <inoue15451116@gmail.com>
2019-09-04 04:25:41 +0000

    Feature/issue 16

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -1,9 +1,5 @@
 <template>
 <div class="center">
-  <form @submit.prevent="submit">
-    <textarea name="situation" v-model="situation" placeholder="自分にストレスを与えた原因や状態を書いてください"></textarea>
-    <button type="submit">submit</button>
-  </form>
   <p v-for="env in environments" :key="env.id">{{env.situation}}</p>
 </div>
 </template>
@@ -14,23 +10,15 @@ import axios from 'axios';
 const URL_BASE = process.env.VUE_APP_ORIGIN
 
 export default {
-  name: 'From',
+  name: 'Index',
   data(){
     return {
-      situation: '',
       environments: []
     }
   },
   mounted() {
     axios.get(URL_BASE + 'api/v1/environments.json')
     .then(response => (this.environments = response.data))
-  },
-  methods: {
-    submit(){
-      const data = {situation: this.situation};
-      axios.post(URL_BASE + 'api/v1/environments', data)
-      .then((_response)=>{console.log(_response)})
-    }
   }
 }
 </script>
