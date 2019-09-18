f34f7da0e2fd9174a3a2de2e25cb58d98bd91a54
H I <inoue15451116@gmail.com>
2019-09-18 04:34:32 +0000

    Feature/issue 20

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -3,6 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
 			origins '192.168.99.100:8080' , 'localhost:8080'
-      resource '/*', headers: :any, methods: [:get, :post, :put, :options]
+      resource '/*', headers: :any, methods: [:get, :post, :put, :delete, :options]
     end
 end
