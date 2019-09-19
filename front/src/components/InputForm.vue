4c36b1440d5f0b9e7ed3bf55c9c2eef7635a4598
HI <inoue15451116@gmail.com>
2019-09-19 15:34:30 +0900

    [WIP]Emotionフォームの送信と削除の形だけ作成仕組み等はまだ未実装

--- front/src/components/InputForm.vue
+++ front/src/components/InputForm.vue
@@ -4,6 +4,18 @@
     <textarea name="situation" v-model="situation" placeholder="自分にストレスを与えた原因や状態を書いてください"></textarea>
     <button type="submit">submit</button>
   </form>
+  <ul>
+    <li v-for="todo in todos">{{ todo }}</li>
+  </ul>
+  <form v-on:submit.prevent="addItem">
+    <div class="form-group">
+        <div class="input-group">
+            <input type="text" v-model="newItem" class="form-control">
+            <span><button class="btn btn-primary" type="submit">送信</button></span>
+        </div>
+    </div>
+  </form>
+  <span><button v-on:click="deleteItem">削除</button></span>
 </div>
 </template>
 
@@ -16,7 +28,9 @@ export default {
   name: 'Form',
   data(){
     return {
-      situation: ''
+      situation: '',
+      newItem: '',
+      todos: []
     }
   },
   methods: {
@@ -24,6 +38,16 @@ export default {
       const data = {situation: this.situation};
       axios.post(URL_BASE + 'api/v1/environments', data)
       // .then((_response)=>{console.log(_response)})
+    },
+    addItem(){
+      this.todos.push(this.newItem);
+      this.newItem = '';
+      console.log(this.todos.length)
+      console.log(this.todos)
+      console.log(this.newItem)
+    },
+    deleteItem(){
+      this.todos.pop()
     }
   }
 }
