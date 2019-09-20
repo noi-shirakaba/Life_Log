7b0f616ec85f75172f588fc2676a90be0bfcc247
taichi oka <chita123daa@gmail.com>
2019-09-20 05:25:18 +0000

    #14 Vueでログイン

--- back/config/application.rb
+++ back/config/application.rb
@@ -15,5 +15,11 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    # クロスオリジンで POST や PUT を拒否しない
+    config.action_controller.forgery_protection_origin_check = false
   end
 end
