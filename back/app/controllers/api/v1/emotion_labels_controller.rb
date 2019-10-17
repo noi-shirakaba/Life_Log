022d140a15e03173be819e2b1577af3bdba37aef
H I <inoue15451116@gmail.com>
2019-10-17 05:19:08 +0000

    【タスク】送信ボタンを１つにまとめる

--- back/app/controllers/api/v1/emotion_labels_controller.rb
+++ back/app/controllers/api/v1/emotion_labels_controller.rb
@@ -1,5 +1,22 @@
 module Api::V1
 	class EmotionLabelsController < ApplicationController
-		protect_from_forgery
+		skip_before_action :authenticate!, only: [:index, :create, :update, :destroy]
+		protect_from_forgery :except => [:create]
+
+		def create
+			EmotionLabel.transaction do
+				emotions_category_params["category"].each do |emotion_category|
+					@emotion_category = EmotionLabel.new(category: emotion_category)
+					@emotion_category.save!
+				end
+			end
+				response_success(:emotion_label, :create)
+			rescue => e
+				response_bad_request
+		end
+
+		def emotions_category_params
+      params.require(:emotion_label).permit(category: [])
+		end
 	end
 end
