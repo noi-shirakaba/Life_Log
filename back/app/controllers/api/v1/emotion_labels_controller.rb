25ead5a288e83f66de8e8af84979bc105be3102f
HI <inoue15451116@gmail.com>
2019-12-17 21:27:58 +0900

    複数形にしたのとループから外した

--- back/app/controllers/api/v1/emotion_labels_controller.rb
+++ back/app/controllers/api/v1/emotion_labels_controller.rb
@@ -1,15 +1,15 @@
 module Api::V1
 	class EmotionLabelsController < ApplicationController
 		def create
-			@emotion_label_id = []
+			@emotion_label_ids = []
+			@emotion = Emotion.new(environment_id: emotions_params["id"])
 			EmotionLabel.transaction do
 				emotions_params["category"].each do |emotion_category|
-					@emotion = Emotion.new(environment_id: emotions_params["id"])
-					@emotion_label_id.push(EmotionLabel.new(category: emotion_category).tap(&:save!).id)
+					@emotion_label_ids.push(EmotionLabel.new(category: emotion_category).tap(&:save!).id)
 					@emotion.save!
 				end
 			end
-				render :json =>{"emotion": @emotion, "emotion_label": @emotion_label_id}
+				render :json =>{"emotion": @emotion, "emotion_label": @emotion_label_ids}
 			rescue => e
 				response_bad_request
 		end
