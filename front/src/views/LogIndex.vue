a519dd4726d1ee1a9d2dd70147336fef2d7272ad
HI <inoue15451116@gmail.com>
2020-03-04 15:28:55 +0900

    ヘッダーのリンクを追加

--- front/src/views/LogIndex.vue
+++ front/src/views/LogIndex.vue
@@ -1,11 +1,16 @@
 <template>
-<Index />
+<v-app>
+	<IndexLink />
+	<Index />
+</v-app>
 </template>
 
 <script>
+import IndexLink from '@/components/Link.vue'
 import Index from '@/components/Index.vue'
 export default {
 	components: {
+		IndexLink,
 		Index
 	}
 }
