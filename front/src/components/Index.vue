7442f226151d0c33b5aa65f5d6f74d11903818aa
HI <inoue15451116@gmail.com>
2020-03-04 10:58:35 +0900

    ページネーション作成

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -1,8 +1,17 @@
 <template>
 <div class="center">
-  <ul v-for="env in environments" :key="env.id">
+  <ul v-for="env in getItems" :key="env.id">
     <li><router-link :to="{ name: 'Show', params: { id: env.id }}">{{env.situation}}</router-link></li>
   </ul>
+  <v-pagination v-model="currentPage" :length="getPageCount" :total-visible="7">
+    <paginate
+      :page-range="3"
+      :margin-pages="2"
+      :click-handler="clickCallback"
+      :container-class="'pagination'"
+      :page-class="'page-item'">
+    </paginate>
+  </v-pagination>
 </div>
 </template>
 
@@ -15,13 +24,23 @@ const URL_BASE = process.env.VUE_APP_ORIGIN
 export default {
   data(){
     return {
-      environments: []
+      environments: [],
+      parPage: 5,
+      currentPage: 1
     }
   },
   computed: {
     ...mapGetters('auth', [
       'getToken',
-    ])
+    ]),
+    getItems: function() {
+      let current = this.currentPage * this.parPage;
+      let start = current - this.parPage;
+      return this.environments.slice(start, current);
+     },
+     getPageCount: function() {
+      return Math.ceil(this.environments.length / this.parPage);
+     }
   },
   mounted() {
     axios.get(URL_BASE + 'api/v1/environments.json', {
@@ -31,6 +50,11 @@ export default {
 		})
     .then(response => (this.environments = response.data))
   },
+  methods: {
+    clickCallback: function (pageNum) {
+      this.currentPage = Number(pageNum);
+    }
+  }
 }
 </script>
 
