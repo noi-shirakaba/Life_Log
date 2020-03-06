23129a0b2c65f0f4d9b8856217e8278776588c4d
HI <inoue15451116@gmail.com>
2020-03-06 13:50:57 +0900

    色などCSSにて設定

--- front/src/components/Show.vue
+++ front/src/components/Show.vue
@@ -3,27 +3,27 @@
 <div id="show">
   <h1 class="show-title">詳細</h1>
   <v-container>
-    <h2 class="index-day">◯記録日</h2>
+    <h2 class="show-subtitle">◯記録日</h2>
     <h2 v-if="data.environment">{{data.environment.created_at}}</h2>
-    <h2>◯環境</h2>
+    <h2 class="show-subtitle">◯環境</h2>
     <h2 v-if="data.environment">{{ data.environment.situation }}</h2>
-    <h2>◯ストレス反応</h2>
-    <h3>・思考やイメージ</h3>
+    <h2 class="show-subtitle">◯ストレス反応</h2>
+    <h3 class="category-title">・思考やイメージ</h3>
     <ul v-for="thought in data.thought" :key="thought.id">
       <li>{{thought.thought_content}}</li>
     </ul>
-    <h3>・感情や気分</h3>
+    <h3 class="category-title">・感情や気分</h3>
     <ul v-for="emoLabel in data.emotion_label" :key="`first-${emoLabel.id}`">
       <li>{{emoLabel.category}}</li>
     </ul>
     <ul v-for="emoEmoLabel in data.emotions_emotion_label" :key="`two-${emoEmoLabel.id}`">
       <li>{{emoEmoLabel.percent}}%</li>
     </ul>
-    <h3>・身体の反応</h3>
+    <h3 class="category-title">・身体の反応</h3>
     <ul v-for="reaction in data.reaction" :key="`three-${reaction.id}`">
       <li>{{reaction.content}}</li>
     </ul>
-    <h3>・行動</h3>
+    <h3 class="category-title">・行動</h3>
     <ul v-for="action in data.action" :key="`four-${action.id}`">
       <li>{{action.action_category}}</li>
     </ul>
@@ -68,5 +68,34 @@ export default {
 </script>
 
 <style scoped>
-
+h1 {
+  color: var(--maincolor);
+  text-align: center;
+}
+h2 {
+  color: var(--maincolor);
+  text-align: left;
+  margin-left: 100px;
+}
+#show ul {
+  list-style: none;
+}
+#show li {
+  margin-left: 100px;
+  margin-bottom: 20px;
+}
+.show-title {
+  margin-top: 40px;
+}
+.category-title {
+  color: var(--maincolor);
+}
+.edit-button a {
+  text-decoration: none;
+  color: var(--maincolor);
+}
+.back-button a {
+  text-decoration: none;
+  color: var(--maincolor);
+}
 </style>
