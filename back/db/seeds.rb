3c0bd084cd22063f9ab81710a2c40416885a3f3d
HI <inoue15451116@gmail.com>
2021-07-03 00:23:35 +0900

    ログインができるようになる

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#! /usr/bin/env ruby
 (1..5).each do |i|
   pp User.create(
     name: "user#{i}",
